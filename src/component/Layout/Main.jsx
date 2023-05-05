4e83a03c9de365dc1c1d841b8841449ff012c509
Ferdows Ahmmed <ferdows962@gmail.com>
2023-05-05 13:01:41 +0600

    spinner added

--- src/component/Layout/Main.jsx
+++ src/component/Layout/Main.jsx
@@ -4,15 +4,38 @@ import { Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../Footer/Footer';
 import  { Toaster } from 'react-hot-toast';
+import { useState, useEffect } from 'react';
+import HashLoader from "react-spinners/HashLoader";
 
 
 const Main = () => {
+    const [loading,setLoading]=useState(false);
+    useEffect(()=>{
+        setLoading(true)
+        setTimeout(()=>{
+            setLoading(false)
+        },5000)
+    },[]);
     return (
-        <div className='position-relative'>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-            <Toaster />
+        <div >
+            {
+            loading ? 
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>     
+      <HashLoader
+      color={'#36d7b7'}
+      loading={loading}
+      size={100}
+      speedMultiplier={1}
+      />
+      </div>
+            :
+            <div>
+                <Header></Header>
+                <Outlet></Outlet>
+                <Footer></Footer>
+                <Toaster />
+            </div>
+            }
         </div>
     );
 };
